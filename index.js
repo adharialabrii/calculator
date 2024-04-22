f85d7123ee5902741bcb82939938d37a512e64ec
adharialabrii <adharialabri9@gmail.com>
2024-04-22 18:48:08 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const display=document.getElementById("display")
+
+
+function displayy(character){
+
+    display.value+=character;
+
+
+}
+
+
+function clearDisplay(){
+
+    display.value=""
+
+}
+
+
+function calculate(){ /*own built in calculaor  */
+
+try{
+    display.value=eval(display.value)
+}
+
+catch(error){
+    display.value="Error"
+}
+    
+
+}
+
+/*check error */
